ecadfa333169734cf90f64787de634b1224d5ac2
Arina Gaitova <arinagaitova@gmail.com>
2026-09-18 23:57:33 +0500

    Task0

--- Tyuiu.GaitovaAA.Sprint1.Task0.V8/DataService.cs
+++ Tyuiu.GaitovaAA.Sprint1.Task0.V8/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.GaitovaAA.Sprint1.Task0.V8.Lib
 {
-    public class DataService : ISprint1Task0V8
+    public class DataService : ISprint1Task0V0
     {
         public double Calculate()
         {
